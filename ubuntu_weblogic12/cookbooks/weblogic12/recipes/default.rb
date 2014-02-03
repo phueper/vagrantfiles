3a1ef2fbae46976c3a40a4eb457393861abab20f
Pattrick Hueper <phueper@hueper.net>
2014-02-03 20:48:59 +0100

    more weblogic cookbook
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- ubuntu_weblogic12/cookbooks/weblogic12/recipes/default.rb
+++ ubuntu_weblogic12/cookbooks/weblogic12/recipes/default.rb
@@ -43,6 +43,7 @@ execute "install wls12" do
   user "vagrant"
   group "vagrant"
   cwd "/home/vagrant"
+  creates "/home/vagrant/weblog12/wls12120"
   command "mvn -f/vagrant/wls12_install/pom.xml com.oracle.weblogic:wls-maven-plugin:install | tee mvn_wls_install.out.txt "
 end
 
@@ -50,7 +51,23 @@ execute "create-domain wls12" do
   user "vagrant"
   group "vagrant"
   cwd "/home/vagrant"
+  creates "/home/vagrant/weblog12/mydomain"
   command "mvn -f/vagrant/wls12_install/pom.xml com.oracle.weblogic:wls-maven-plugin:create-domain | tee mvn_wls_create-domain.out.txt"
+end
+
+#setDomainEnv has wrong JAVA_HOME
+execute "fix JAVA_HOME" do
+  user "vagrant"
+  group "vagrant"
+  cwd "/home/vagrant"
+  command "sed -e 's/java-6-openjdk-amd64/java-6-openjdk-amd64\\/jre/g' -i weblogic12/mydomain/bin/setDomainEnv.sh 2>&1 | tee sed_JAVA_HOME.out.txt"
+end
+
+execute "fix permissions" do
+  user "vagrant"
+  group "vagrant"
+  cwd "/home/vagrant"
+  command "find weblogic12 -iname \"*.sh\" | xargs chmod u+x | tee chmod_ux_sh.out.txt"
 end
 
 log "wls12 installed in /home/vagrant/weblogic12, run /home/vagrant/weblogic12/mydomain/startWeblogic.sh to start"
